c2f81839c24d89545c793cb7b2074e99206b879a
adithya-1 <adithyaaravi10@gmail.com>
2021-01-04 20:47:48 +0530

    Added global and landing

---
+++ lib/global.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color backGroundColor = Color(0xff222629);
+Color yellowColor = Color(0xffffca60);
+Color chosenColor = yellowColor;
+Color lightGreyColor = Color(0xff2b2f32);
---
+++ lib/landing.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:pyramid_web/global.dart';
+
+class Landing extends StatefulWidget {
+  Landing({Key key}) : super(key: key);
+
+  @override
+  _LandingState createState() => _LandingState();
+}
+
+class _LandingState extends State<Landing> {
+  double height;
+  double width;
+  @override
+  Widget build(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    return Scaffold(
+        backgroundColor: backGroundColor,
+        body: Column(children: [
+          Row(
+            children: [
+              SizedBox(width: width * 0.01),
+              greyBoxes(height * 0.1, width * 0.03),
+              SizedBox(width: width * 0.01),
+              greyBoxes(height * 0.1, width * 0.03)
+            ],
+          ),
+          Row(children: [colorChangingBox(height * 0.1, width * 0.03, 'P')])
+        ]));
+  }
+
+  Widget colorChangingBox(height, width, text) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: new BoxDecoration(
+        color: chosenColor,
+      ),
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(color: backGroundColor, fontFamily: 'OCRA'),
+        ),
+      ),
+    );
+  }
+
+  Widget greyBoxes(height, width) {
+    return Container(
+        width: width,
+        height: height,
+        decoration: new BoxDecoration(
+          color: lightGreyColor,
+        ));
+  }
+}
