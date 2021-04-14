5c9f4ff7cc61c4682d54f20fc045dc82323eb608
Dhruv Shetty <dhruvshetty3@gmail.com>
2021-04-15 01:39:39 +0530

    Deleting Flutter

--- lib/global.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color backGroundColor = Color(0xff222629);
-Color yellowColor = Color(0xffffca60);
-Color chosenColor = yellowColor;
-Color lightGreyColor = Color(0xff2b2f32);
-Color lightTextColor = Color(0xffadadad);
